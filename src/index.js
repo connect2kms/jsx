45963891a156988b5ee7ea0203572ed445cddbe3
Karthik Mohanasundaram <connect2kms@outlook.com>
2022-01-08 20:47:27 -0500

    jsx basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+// import react and reactdom lib
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//reload auto
+if (module.hot) {
+    module.hot.accept();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// create react compoent
+// const App = function () {
+//     return <div> Hi there ! </div>
+// }
+
+const App = () => {
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: "blue", color: "white" }}>
+                Submit
+            </button>
+        </div>
+    );
+};
+
+//Rake the react ompoennt nad show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
